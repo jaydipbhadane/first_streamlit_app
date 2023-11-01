18bbca2c34e5e91cb6d2cfe9b2e1a22f700c8c49
Jaydip Bhadane <75785278+jaydipbhadane@users.noreply.github.com>
2023-11-01 10:15:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 import pandas
-streamlit.title("YashOne Food App")
-streamlit.header("Get your food at your Door step...")
-streamlit.text("what food We Serves :")
+streamlit.title("YashOne Online Fruit Stall")
+streamlit.header("Get your fruits at your Door step...")
+streamlit.text("what fruits We delivers :")
 food = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(food)
-streamlit.multiselect("Pick the Food you would like to it : ",list(food['Fruit']))
+streamlit.multiselect("Pick the Fruits you would like to it : ",list(food['Fruit']))
