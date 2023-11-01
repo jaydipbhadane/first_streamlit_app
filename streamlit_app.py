d073184726f9299199a78152cb81378ba07b1095
Jaydip Bhadane <75785278+jaydipbhadane@users.noreply.github.com>
2023-11-01 10:02:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import pandas
 streamlit.title("YashOne Food App")
 streamlit.header("Get your food at your Door step...")
 streamlit.text("what food We Serves :")
-food = pandas.read_csv(uni-lab-files.s3.us-west-2-amazonaws.com/dabw/fruit_macros.txt)
+food = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(food)
