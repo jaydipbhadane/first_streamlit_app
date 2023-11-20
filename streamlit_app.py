1e413412f9e9f25893b8875223669a594c83d528
Jaydip Bhadane <75785278+jaydipbhadane@users.noreply.github.com>
2023-11-20 19:11:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,4 @@ food = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fr
 streamlit.dataframe(food)
 selected_fruits = streamlit.multiselect("Pick the Fruits you would like to it : ",list(food['Fruit']))
 streamlit.text("Your Cart")
-cart =  food[selected_fruits]
-streamlit.dataframe(cart)
+streamlit.dataframe(selected_fruits)
