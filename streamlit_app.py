241419b197687257a83af33a62f5a26681b30695
Jaydip Bhadane <75785278+jaydipbhadane@users.noreply.github.com>
2023-11-21 10:16:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit
 import pandas
+
 streamlit.title("YashOne Fruit Shop")
 streamlit.header("Get fruits at your Door step...")
 streamlit.text("what fruits We delivers :")
+
 food = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-food = food.set_index('Fruit')
+#food = food.set_index('Fruit')
 streamlit.dataframe(food)
 selected_fruits = streamlit.multiselect("Pick the Fruits you would like to it : ",list(food.index))
 cart = food.loc[selected_fruits]
